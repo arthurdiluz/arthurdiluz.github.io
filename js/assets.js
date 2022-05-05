fc598d6f2440b664eefeb92b7b9cb75e694cfc42
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2022-05-05 15:25:33 -0300

    update assets.js

--- js/assets.js
+++ js/assets.js
@@ -1,13 +1,13 @@
-window.addEventListener('load', _ => {
-    const infoElement = document.querySelector('#shortInfo')
-    const divInfo = document.createElement('div')
-    const aElement = document.createElement('a')
-    const age = String(moment().diff('1999-07-30', 'years'))
+window.addEventListener("load", (_) => {
+  const infoElement = document.querySelector("#shortInfo");
+  const divInfo = document.createElement("div");
+  const aElement = document.createElement("a");
+  const age = String(moment().diff("1999-07-30", "years"));
 
-    aElement.href = '../static/resume.pdf'
-    aElement.target = '_blank'
-    aElement.text = 'read curriculum vitae'
-    divInfo.textContent = `${age} years old · santa amélia, paraná, brasil`
-    infoElement.appendChild(divInfo)
-    infoElement.appendChild(aElement)
-})
+  aElement.href = "../static/resume.pdf";
+  aElement.target = "_blank";
+  aElement.text = "read curriculum vitae";
+  divInfo.textContent = `${age} years old · bandeirantes, paraná, brasil`;
+  infoElement.appendChild(divInfo);
+  infoElement.appendChild(aElement);
+});
