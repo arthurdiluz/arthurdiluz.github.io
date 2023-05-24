9b42e9e3c0fc1d41fcf8c4dbe173bbe8852e6f74
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-05-24 16:45:21 -0300

    update text

--- js/assets.js
+++ js/assets.js
@@ -7,7 +7,7 @@ window.addEventListener("load", (_) => {
   aElement.href = "../static/resume.pdf";
   aElement.target = "_blank";
   aElement.text = "read curriculum vitae";
-  divInfo.textContent = `${age} years old · bandeirantes, paraná, brasil`;
+  divInfo.textContent = `${age} years old · greater campinas, são paulo, brasil`;
   infoElement.appendChild(divInfo);
   infoElement.appendChild(aElement);
 });
